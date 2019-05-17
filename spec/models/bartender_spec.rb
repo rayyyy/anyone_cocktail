e4f631f2e70b79a94f2e548fa4cf211c70428010
rayyyy <nraavy@gmail.com>
2019-05-17 16:08:39 +0900

    ちょっとテストを整えた

--- spec/models/bartender_spec.rb
+++ spec/models/bartender_spec.rb
@@ -13,7 +13,37 @@ require 'rails_helper'
 
 RSpec.describe Bartender, type: :model do
 
+  let!(:params) {
+    {
+      name: '新米バーテンダーくん＠名前は２０文字まで',
+      sex: 1
+    }
+  }
+
   it 'is valid all parameter' do
+    bartender = Bartender.new(params)
+    expect(bartender).to be_valid
+  end
+
+  it 'is invalid name empty' do
+    params[:name] = ''
+    bartender = Bartender.new(params)
+    expect(bartender).to be_invalid
+  end
+
+  it 'is invalid name nil' do
+    params[:name] = nil
+    bartender = Bartender.new(params)
+    expect(bartender).to be_invalid
+  end
+
+  it 'is invalid name long text' do
+    params[:name] = '私の名前は２１文字です＠＠＠＠＠＠＠＠＠＠'
+    bartender = Bartender.new(params)
+    expect(bartender).to be_invalid
+  end
+
+  it 'is invalid sex' do
     bartender = Bartender.new(
       name: '新米バーテンダーくん',
       sex: 1
@@ -21,7 +51,29 @@ RSpec.describe Bartender, type: :model do
     expect(bartender).to be_valid
   end
 
-  it 'is invalid name'
-  it 'is invalid sex'
+
+  it 'is invalid sex zero' do
+    bartender = Bartender.new(
+      name: '新米バーテンダーくん',
+      sex: 0
+    )
+    expect(bartender).to be_invalid
+  end
+
+  it 'is invalid sex 3' do
+    bartender = Bartender.new(
+      name: '新米バーテンダーくん',
+      sex: 3
+    )
+    expect(bartender).to be_invalid
+  end
+
+  it 'is invalid sex nil' do
+    bartender = Bartender.new(
+      name: '新米バーテンダーくん',
+      sex: nil
+    )
+    expect(bartender).to be_invalid
+  end
 
 end
