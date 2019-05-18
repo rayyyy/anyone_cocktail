0b4e0f9e80b7c21d9879164ad82530765dcc6c11
rayyyy <nraavy@gmail.com>
2019-05-18 17:55:07 +0900

    mypageと認証機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :auth_users
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get 'my_page', to: 'my_page#index'
   get '/bartenders/:id', to: 'bartenders#show'
 end
